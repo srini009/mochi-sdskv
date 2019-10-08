b29ca586925663f8748657bc9c4c40c33be98895
Matthieu Dorier <mdorier@anl.gov>
2019-10-08 16:29:34 +0100

    changed implementation of ds_bulk_t

--- src/bulk.h
+++ src/bulk.h
@@ -6,32 +6,132 @@
 #include <stddef.h>
 #include "kv-config.h"
 //#include <boost/functional/hash.hpp>
+#include "fnv1a.h"
 #include <vector>
 #include <string>
 
-// implementation is std::vector<char> specific
-// typedef is for convenience
-typedef std::vector<char> ds_bulk_t;
+class ds_bulk_t {
+    
+    bool   _owns_data = false;
+    size_t _size      = 0;
+    char*  _data      = nullptr;
+
+    public:
+
+    ds_bulk_t() = default;
+
+    ds_bulk_t(const char* begin, const char* end)
+    : _size(std::distance(begin, end))
+    , _data(const_cast<char*>(begin)) {}
+
+    ds_bulk_t(const char* data, size_t size)
+    : _size(size)
+    , _data(const_cast<char*>(data)) {}
+
+    ds_bulk_t(size_t size)
+    : _owns_data(true)
+    , _size(size)
+    , _data((char*)malloc(size)) {}
+
+    ~ds_bulk_t() {
+        if(_owns_data) free(_data);
+    }
+
+    ds_bulk_t(const ds_bulk_t& other)
+    : _owns_data(true)
+    , _size(other._size) {
+        _data = (char*)malloc(_size);
+        memcpy(_data, other._data, _size);
+    }
+
+    ds_bulk_t(ds_bulk_t&& other)
+    : _owns_data(other._owns_data)
+    , _size(other._size)
+    , _data(other._data) {
+        other._owns_data = false;
+        other._size = 0;
+        other._data = nullptr;
+    }
+
+    ds_bulk_t& operator=(const ds_bulk_t& other) {
+        if(&other == this) return *this;
+        if(_owns_data) free(_data);
+        _owns_data = true;
+        _size = other._size;
+        _data = (char*)malloc(_size);
+        memcpy(_data, other._data, _size);
+        return *this;
+    }
+
+    ds_bulk_t& operator=(ds_bulk_t&& other) {
+        if(&other == this) return *this;
+        if(_owns_data) free(_data);
+        _owns_data = other._owns_data;
+        _size = other._size;
+        _data = other._data;
+        other._owns_data = false;
+        other._size = 0;
+        other._data = nullptr;
+        return *this;
+    }
+
+    const char* data() const {
+        return _data;
+    }
+
+    char* data() {
+        return _data;
+    }
+
+    size_t size() const {
+        return _size;
+    }
+
+    bool operator==(const ds_bulk_t& other) const {
+        if(_size != other._size) return false;
+        if(_data == other._data) return true;
+        return 0 == memcmp(_data, other._data, _size);
+    }
+
+    bool operator!=(const ds_bulk_t& other) const {
+        return !(*this == other);
+    }
+
+    bool operator<(const ds_bulk_t& other) const {
+        if(_size == other._size) {
+            if(_data == other._data) return false;
+            int r = memcmp(_data, other._data, _size);
+            return r < 0;
+        } else {
+            auto s = std::min(_size, other._size);
+            if(_data == other._data)
+                return _size < other._size;
+            int r = memcmp(_data, other._data, _size);
+            if(r < 0) return true;
+            if(r > 0) return false;
+            return _size < other._size;
+        }
+    }
+};
 
 struct ds_bulk_hash {
-  size_t operator()(const ds_bulk_t &v) const {
-    size_t hash = 0;
-    hash = std::hash<std::string>() ( std::string( v.begin(), v.end() ) );
-        //boost::hash_range(hash, v.begin(), v.end());
-    return hash;
-  }
+    size_t operator()(const ds_bulk_t &v) const {
+        auto hashfn = fnv1a_t<8 * sizeof(std::size_t)> {};
+        hashfn.update(v.data(), v.size());
+        return hashfn.digest();
+    }
 };
 
 struct ds_bulk_equal {
-  bool operator()(const ds_bulk_t &v1, const ds_bulk_t &v2) const {
-    return (v1 == v2);
-  }
+    bool operator()(const ds_bulk_t &v1, const ds_bulk_t &v2) const {
+        return (v1 == v2);
+    }
 };
 
 struct ds_bulk_less {
-  bool operator()(const ds_bulk_t &v1, const ds_bulk_t &v2) const {
-    return (v1 < v2);
-  }
+    bool operator()(const ds_bulk_t &v1, const ds_bulk_t &v2) const {
+        return (v1 < v2);
+    }
 };
 
 #endif // bulk_h
